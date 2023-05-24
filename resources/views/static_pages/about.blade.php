a899aa46e7bb64e21c6f5dd185830fefd4ae3a5c
luo2009 <luoxiaoyu1010@gmail.com>
2023-05-24 04:40:40 +0000

    模板切分

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.default')
+@section('title',"关于")
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Weibo App</title>
-</head>
-
-<body>
-  <h1>关于页</h1>
-</body>
-
-</html>
+@section('content')
+<h1>关于页</h1>
+@endsection
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.default')
+@section('title',"帮助")
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Weibo App</title>
-</head>
-
-<body>
-  <h1>帮助页</h1>
-</body>
-
-</html>
+@section('content')
+<h1>帮助页</h1>
+@endsection
