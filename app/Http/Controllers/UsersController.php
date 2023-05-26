3b5dbfac699e2326b12b90102c5f34b77f0a5943
luo2009 <luoxiaoyu1010@gmail.com>
2023-05-26 02:48:16 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,18 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
     public function create()
     {
         return view("users.create");
@@ -21,14 +33,14 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|unique:users|max:50',
-            'email' => "required|email|unique:users|max:255",
+            'name'     => 'required|unique:users|max:50',
+            'email'    => "required|email|unique:users|max:255",
             'password' => "required|confirmed|min:6"
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
+            'name'     => $request->name,
+            'email'    => $request->email,
             'password' => bcrypt($request->password)
         ]);
 
@@ -39,13 +51,15 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update',$user);
         $this->validate($request, [
-            'name' => 'required|max:50',
+            'name'     => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
 
